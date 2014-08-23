fa694c4c0e37356cf8bde5e4f4cccd9394e5c32f
tektekPush <snatera09@gmail.com>
2014-08-23 15:57:29 -0400

    Updates the view for chapter 1 and 2; completed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # *** Added to render specifice stylesheets
+  #config.assets.precompile += %w( custom.css , chapters.css  )
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
